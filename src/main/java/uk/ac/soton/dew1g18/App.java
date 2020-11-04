7e389591637621457db624629da80505cfbbe1b4
Dew1g18 <Davewadd3@gmail.com>
2020-11-04 17:59:45 +0000

    started hybrid images, created some stub methods to break up my work process, need to work out how to find sigma automatically to be cool

--- src/main/java/uk/ac/soton/dew1g18/App.java
+++ src/main/java/uk/ac/soton/dew1g18/App.java
@@ -7,8 +7,10 @@ import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.processing.convolution.Gaussian2D;
 import org.openimaj.image.typography.hershey.HersheyFont;
 
+import java.io.File;
 import java.net.URL;
 
 /**
@@ -32,28 +34,48 @@ public class App {
 //        //Display the image
 //        DisplayUtilities.display(image);
 
+//
+////        float[][] kernel = {{1/9,1/9,1/9},{1/9,1/9,1/9},{1/9,1/9,1/9}};//Averaging (doesnt work)
+////        float[][] kernel = {{-1,0,1}, {0,0,0},{1,0,-1}};//Weak edge detection
+//        float[][] kernel = {{-1,-1,-1},{-1,8,-1}, {-1,-1,-1}};//Strong edge detection
+////        float[][] kernel = {{0,0,0}, {0,1,0}, {0,0,0}};//Identity
+////        float[][] kernel = {{0,-1,0}, {-1,5,-1}, {0,-1,0}};//sharpening
+//
+//
+//        /**
+//         * The following was what i used while testing my convolution with the above kernels from wikipedia.
+//         */
+//        MyConvolution mycon = new MyConvolution( kernel);
+//
+//
+//        try{
+//
+//            FImage image = ImageUtilities.readF(new URL("http://static.openimaj.org/media/tutorial/sinaface.jpg"));
+//            DisplayUtilities.display(image);
+//            //            DisplayUtilities.display(image);
+//            image.processInplace(mycon);
+//
+//            DisplayUtilities.display(image);
+//
+//
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
 
-//        float[][] kernel = {{1/9,1/9,1/9},{1/9,1/9,1/9},{1/9,1/9,1/9}};//Averaging (doesnt work)
-//        float[][] kernel = {{-1,0,1}, {0,0,0},{1,0,-1}};//Weak edge detection
-        float[][] kernel = {{-1,-1,-1},{-1,8,-1}, {-1,-1,-1}};//Strong edge detection
-//        float[][] kernel = {{0,0,0}, {0,1,0}, {0,0,0}};//Identity
-//        float[][] kernel = {{0,-1,0}, {-1,5,-1}, {0,-1,0}};//sharpening
 
-        MyConvolution mycon = new MyConvolution( kernel);
+        /**
+         * Hybrid image practice space
+         */
+        try {
+            MBFImage lowImage = ImageUtilities.readMBF(new File("C:\\Users\\davew\\openimaj-coursework2\\src\\main\\java\\uk\\ac\\soton\\dew1g18\\hybrid-images\\bird.bmp"));
+            MBFImage highImage = ImageUtilities.readMBF(new File("C:\\Users\\davew\\openimaj-coursework2\\src\\main\\java\\uk\\ac\\soton\\dew1g18\\hybrid-images\\fish.bmp"));
 
-
-        try{
-
-            FImage image = ImageUtilities.readF(new URL("http://static.openimaj.org/media/tutorial/sinaface.jpg"));
-            DisplayUtilities.display(image);
-            //            DisplayUtilities.display(image);
-            image.processInplace(mycon);
-
-            DisplayUtilities.display(image);
-
+            MyHybridImages mh = new MyHybridImages();
+            mh.makeHybrid(lowImage, 1f, highImage, 1f);
 
         }catch(Exception e){
             e.printStackTrace();
         }
+
     }
 }
--- src/main/java/uk/ac/soton/dew1g18/MyHybridImages.java
+++ src/main/java/uk/ac/soton/dew1g18/MyHybridImages.java
@@ -1,4 +1,62 @@
 package uk.ac.soton.dew1g18;
 
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.convolution.Gaussian2D;
+
 public class MyHybridImages {
+
+    /**
+     * Compute a hybrid image combining low-pass and high-pass filtered images
+     *
+     * @param lowImage
+     *            the image to which apply the low pass filter
+     * @param lowSigma
+     *            the standard deviation of the low-pass filter
+     * @param highImage
+     *            the image to which apply the high pass filter
+     * @param highSigma
+     *            the standard deviation of the low-pass component of computing the
+     *            high-pass filtered image
+     * @return the computed hybrid image
+     */
+    public static MBFImage makeHybrid(MBFImage lowImage, float lowSigma, MBFImage highImage, float highSigma) {
+        //implement your hybrid images functionality here.
+        //Your submitted code must contain this method, but you can add
+        //additional static methods or implement the functionality through
+        //instance methods on the `MyHybridImages` class of which you can create
+        //an instance of here if you so wish.
+        //Note that the input images are expected to have the same size, and the output
+        //image will also have the same height & width as the inputs.
+        DisplayUtilities.display(lowImage);
+
+        convolve(getKernel(1),lowImage );
+
+        DisplayUtilities.display(lowImage);
+
+
+
+        return lowImage;
+    }
+
+
+    protected static float[][] getKernel(int sigma){
+        int size = (int) (8.0f*sigma+ 1.0f );
+        if (size%2==0){size++;} //Size must be odd for kernel apparently...
+        return Gaussian2D.createKernelImage(size, sigma).pixels;
+    }
+
+    protected static MBFImage convolve(float[][] kernel, MBFImage image){
+        MyConvolution myCon = new MyConvolution(kernel);
+        image.processInplace(myCon);
+        return image;
+    }
+
+    protected static float getLowPassSigma(MBFImage image){
+        return 1f;
+    }
+
+    private float getHighPassSigma(MBFImage image){
+        return 1f;
+    }
 }
